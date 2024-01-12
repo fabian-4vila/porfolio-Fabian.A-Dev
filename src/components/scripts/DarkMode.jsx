92ff2064ccd2c28aa477b4566518bb720df6ccc2
Fabian_Avila <116823446+fabian-4vila@users.noreply.github.com>
2024-01-11 23:27:02 -0500

    new8-commit

--- src/components/scripts/DarkMode.jsx
+++ src/components/scripts/DarkMode.jsx
@@ -22,7 +22,7 @@ const DarkMode = () => {
 
     const applyOriginalColors = () => {
         document.documentElement.style.setProperty('--color-BG-A', 'linear-gradient(216deg,rgba(178,178,178,0.05) 0%,rgba(178,178,178,0.05) 25%,rgba(213,213,213,0.05) 25%,rgba(213,213,213,0.05) 38%,rgba(32,32,32,0.05) 38%,rgba(32,32,32,0.05) 75%,rgba(219,219,219,0.05) 75%,rgba(219,219,219,0.05) 100%),linear-gradient(44deg,rgba(127,127,127,0.05) 0%,rgba(127,127,127,0.05) 34%,rgba(43,43,43,0.05) 34%,rgba(43,43,43,0.05) 57%,rgba(230,230,230,0.05) 57%,rgba(230,230,230,0.05) 89%,rgba(120,120,120,0.05) 89%,rgba(120,120,120,0.05) 100%),linear-gradient(241deg,rgba(200,200,200,0.05) 0%,rgba(200,200,200,0.05) 14%,rgba(46,46,46,0.05) 14%,rgba(46,46,46,0.05) 60%,rgba(10,10,10,0.05) 60%,rgba(10,10,10,0.05) 69%,rgba(91,91,91,0.05) 69%,rgba(91,91,91,0.05) 100%),linear-gradient(249deg,rgba(74,74,74,0.05) 0%,rgba(74,74,74,0.05) 32%,rgba(107,107,107,0.05) 32%,rgba(107,107,107,0.05) 35%,rgba(53,53,53,0.05) 35%,rgba(53,53,53,0.05) 51%,rgba(74,74,74,0.05) 51%,rgba(74,74,74,0.05) 100%),linear-gradient(92deg,hsl(214,0%,89%),hsl(214,0%,89%))');
-        document.documentElement.style.setProperty('--color-Name', 'linear-gradient(270deg, #A30D0D 0%, #24105A 100%)');
+        document.documentElement.style.setProperty('--color-Name', 'linear-gradient(270deg, #A30D0Dd3 0%, #24105Ad3 100%)');
         document.documentElement.style.setProperty('--color-T1', '#8D74D0b8');
         document.documentElement.style.setProperty('--color-T3', '#8D74D0b8');
         document.documentElement.style.setProperty('--color-T2', '#ffffff80');
